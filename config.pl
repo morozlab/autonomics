20f278e64806c1028ee2ad3c3b23e2f5d48a4510
Peter Williams <pwilliams@c10node12.acis.ufl.edu>
2013-12-17 22:22:38 -0500

    .

---
+++ config.pl
@@ -0,0 +1,6 @@
+queryPath => "/var/www/html/neurobase/slimebase2/results",
+dbPath => "/var/www/html/neurobase/seq_view/database/",
+dsn => "dbi:mysql:database=moroz_lab;host=localhost",
+db_username => "moroz_lab",
+db_password => "Whitney2011",
+[download]
