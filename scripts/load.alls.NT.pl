57679a6192ce6ee6b241459a2ede4becb22636b3
Peter Williams <pwilliams@c10node12.acis.ufl.edu>
2014-01-19 10:49:09 -0500

    .

--- scripts/load.alls.NT.pl
+++ scripts/load.alls.NT.pl
@@ -11,7 +11,7 @@ hlc4
 );
 
 foreach my $p (@projs) {
- $cmd = "loadnb.pl $p NT ALL";
+ $cmd = "load.project.pl $p NT ALL";
   print "$cmd\n";
   system ($cmd);
   if ( $? ) { die "Command failed: $cmd: $!"; }
